58d48b294b01d7fc2a0639b660cffa52b2c9af74
apm144 <marencic144@gmail.com>
2023-12-27 06:30:24 -0500

    Finished day 10.  Solving for full puzzle is a little slow.

--- scripts/day10.py
+++ scripts/day10.py
@@ -1,4 +1,6 @@
 import numpy as np
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 # Read in data, new item for each line
 f = open(r'..\inputs\input_day10.txt')
@@ -75,7 +77,8 @@ test_answer_5 = 8
 test_answer_6 = 10
 
 
-
+# Function to get the next coordinate of the animal from the previous coordinate,
+# current coordinate, and current coordinate pipe type.
 def get_next_corr(pre_corr, curr_corr, pipe_type):
     next_corr = []
     if pipe_type == '-':
@@ -129,15 +132,23 @@ def get_next_corr(pre_corr, curr_corr, pipe_type):
     return next_corr
 
 
-def solve_day_part_1(data):
+def solve_day(data):
 
     grid = []
     start = []
+    # Looping over each row to create the grid
+    # Grid will have [row number, col number] coordinates
     for i, row in enumerate(data):
         grid.append([j for j in row])
+        # Identifying start coordinate
         if 'S' in row:
             start = [i, [j for j, k in enumerate(row) if k == 'S'][0]]
 
+    # Determine the next coordinate the animal is moving in from the start
+    # Because start will have two possible directions, the next_corr variable
+    # will be updated twice.  It does not matter which direction it goes.
+    # Will check in each of the four directions and select it for a coordinate
+    # if it has a valid pipe type.
     next_corr = []
     pipe_type = ''
     # Checking up
@@ -161,30 +172,78 @@ def solve_day_part_1(data):
             next_corr = [start[0], start[1] + 1]
             pipe_type = grid[start[0]][start[1] + 1]
 
+    # Initializing variables for the while loop
     counter = 1
+    # Current coordinate where animal is
     curr_corr = next_corr.copy()
+    # Previous coordinate where animal was
     pre_corr = start.copy()
+    # Setting numpy Boolean grid for path of the animal
+    # 1 being a coordinate where the path is.
+    bool_grid = np.zeros((len(grid), len(grid[0]))).astype(int)
+    bool_grid[start[0], start[1]] = 1
+    bool_grid[curr_corr[0], curr_corr[1]] = 1
 
+    # Tracking list of coordinates of the path.  Will be used to make a polygon
     moves = [start.copy(), next_corr.copy()]
+    # Loop through until we encounter the start again.
     while pipe_type != 'S':
+        # Get the next coordinate using previous coordinate, current coordinate,
+        # and pipe type
         next_corr = get_next_corr(pre_corr, curr_corr, pipe_type)
+        # Add next coordinate to polygon list
         moves.append(next_corr.copy())
+        # Update Boolean grid with path coordinate
+        bool_grid[next_corr[0], next_corr[1]] = 1
+        # Increase counter
         counter += 1
+        # Update pipe type, previous coordinate, current coordinate
         pipe_type = grid[next_corr[0]][next_corr[1]]
         pre_corr = curr_corr.copy()
         curr_corr = next_corr.copy()
 
+    # Make polygon
+    polygon = Polygon(moves)
+    inside_counter = 0
+    # Loop over every coordinate in the grid to identify if a coordinate is inside
+    # the path
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            # If a path coordinate, skip it
+            if bool_grid[i, j] == 1:
+                continue
+            # If the sum of the Boolean grid from edge to the current coordinate
+            # is 0, that means it is outside of the path, so it is skipped.
+            # Check for each direction.
+            if sum(bool_grid[i, 0:j + 1]) == 0:
+                continue
+            if sum(bool_grid[0:i + 1, j]) == 0:
+                continue
+            if sum(bool_grid[i, j:]) == 0:
+                continue
+            if sum(bool_grid[i:, j]) == 0:
+                continue
+            # Check if coordinate is inside the path.  If it is, increase counter.
+            point = Point(i, j)
+            if polygon.contains(point):
+                inside_counter += 1
+
     print('Farthest point is , ', int(counter / 2))
-    return int(counter / 2)
+    print('Number of points inside loop, ', inside_counter)
+    return int(counter / 2), inside_counter
 
 
-
-output_test = solve_day_part_1(test_1)
-print('Output equal to test_1 output for part 1, ', output_test == test_answer_1)
-output_test_2 = solve_day_part_1(test_2)
-print('Output equal to test_2 output for part 1, ', output_test_2 == test_answer_2)
-output = solve_day_part_1(lines)
-# output_test_3 = solve_day_part_2(test_3)
-# print('Output equal to test_2 output for part 2, ', output_test_3 == test_answer_3)
-# output_2 = solve_day_part_2(lines)
+output_test = solve_day(test_1)
+print('Output equal to test_1 output for part 1, ', output_test[0] == test_answer_1)
+output_test_2 = solve_day(test_2)
+print('Output equal to test_2 output for part 1, ', output_test_2[0] == test_answer_2)
+output_test_3 = solve_day(test_3)
+print('Output equal to test_3 output for part 2, ', output_test_3[1] == test_answer_3)
+output_test_4 = solve_day(test_4)
+print('Output equal to test_4 output for part 2, ', output_test_4[1] == test_answer_4)
+output_test_5 = solve_day(test_5)
+print('Output equal to test_5 output for part 2, ', output_test_5[1] == test_answer_5)
+output_test_6 = solve_day(test_6)
+print('Output equal to test_6 output for part 2, ', output_test_6[1] == test_answer_6)
+output = solve_day(lines)
 
